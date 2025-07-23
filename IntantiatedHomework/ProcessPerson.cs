9c291acfc913bb073fcaa62479dce7dd356e9946
jblau <jblau@JOSHLAPPY>
2025-07-22 23:28:57 -0600

    Add project files.

---
+++ IntantiatedHomework/ProcessPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantiatedHomework
+{
+    public static class ProcessPerson
+    {
+        public static void GreetPerson(PersonModel person)
+        {
+            Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
+            person.HasBeenGreeted = true;
+        }
+      
+    }
+}
---
+++ IntantiatedHomework/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantiatedHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            string firstName= "";
+
+            
+            
+
+            do
+            {
+                Console.Write("What is your first name (or type exit to stop):");
+                firstName = Console.ReadLine();
+
+                Console.Write("What is your last name: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("What is your age:");
+                string age = Console.ReadLine();
+
+                Console.Write("What is your street address:");
+                string streetAddress = Console.ReadLine();
+
+                Console.Write("What is your city:");
+                string city = Console.ReadLine();
+
+                Console.Write("What is your state:");
+                string state = Console.ReadLine();
+
+                Console.Write("What is your zip code:");
+                string postalCode = Console.ReadLine();
+
+
+
+               
+
+                if (firstName.ToLower() != "exit")
+                {
+                    PersonModel person1 = new PersonModel();
+                    person1.FirstName = firstName;
+                    person1.LastName = lastName;
+                    person1.Age = age;
+                    people.Add(person1);
+
+                    AddressModel address = new AddressModel();
+                    address.StreetAddress = streetAddress;
+                    address.City = city;
+                    address.State = state;
+                    address.PostalCode = postalCode;
+
+
+                }
+
+            } while (firstName.ToLower() != "exit");
+
+            foreach (PersonModel p in people)
+            {
+                ProcessPerson.GreetPerson(p);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
